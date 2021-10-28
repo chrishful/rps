fdee793087adc3513ce6dec15f2894060551a532
chrishful <89863527+chrishful@users.noreply.github.com>
2021-10-28 14:46:19 -0400

    logic done and working

--- js/page.js
+++ js/page.js
@@ -1,4 +1,39 @@
-function test() {
-  let el = document.getElementById('test');
-  el.classList.add("test");
+function compChoice(){
+var compchoice = Math.floor(Math.random()*3);
+console.log("function is working");
+ if(compchoice == 0){
+   document.getElementById('o-img').src ="svg/rock.svg";
+   return 0;
+ }
+ else if(compchoice == 1){
+   document.getElementById('o-img').src ="svg/paper.svg";
+   return 1;
+ }
+ else{
+   document.getElementById('o-img').src ="svg/scissors.svg";
+   return 2;
+ }
+}
+
+function start(player){
+  var computer = compChoice();
+      if(player == computer) document.getElementById('result').innerHTML = "It's a tie";
+      else { switch (player) {
+        case 0:
+        if(computer == 1) document.getElementById('result').innerHTML = "You lose";
+        else document.getElementById('result').innerHTML = "You win!!";
+          break;
+        case 1:
+         if(computer == 2) document.getElementById('result').innerHTML = "You lose";
+         else document.getElementById('result').innerHTML = "You win!!";
+         break;
+        case 2:
+         if(computer == 0) document.getElementById('result').innerHTML = "You lose";
+         else document.getElementById('result').innerHTML = "You win!!";
+         break;
+        default:
+         console.log("there was an error");
+         break;
+      }
+    }
 }
